75d44c417cccb6e590069a4f226bcc8a3025e0ae
bershin <bershin@gmail.com>
2024-08-23 14:39:37 +0100

    Second Commit

--- src/main/java/com/stacksimplify/restservices/springboot_project/SpringbootProjectApplication.java
+++ src/main/java/com/stacksimplify/restservices/springboot_project/SpringbootProjectApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplify.restservices.springboot_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootProjectApplication {
 
